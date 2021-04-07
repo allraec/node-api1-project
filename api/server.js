fa4eb05f8bbb3a4d356a142501c7f5f87658b1ab
Allison Castaneda <allraec@yahoo.com>
2021-04-06 23:46:07 -0500

    GET request users endpoint

--- api/server.js
+++ api/server.js
@@ -1,3 +1,19 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
+const db = require("./users/model");
+const server = express();
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use(express.json());
+
+//Endpoint that returns an array of users.
+server.get("/api/users", (req, res) => {
+    db.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(() => {
+        res.status(500).json({ message: "The users information could not be retrieved" })
+    })
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
