3afa5c2d5bd48d281e9ff37fb5118ccbd226598f
Allison Castaneda <allraec@yahoo.com>
2021-04-07 20:10:32 -0500

    delete and put implemented

--- api/server.js
+++ api/server.js
@@ -46,6 +46,41 @@ server.get("/api/users/:id", (req, res) => {
         .catch(() => {
             res.status(404).json({ message: "The user information could not be retrieved" });
         });
-})
+});
 
+server.delete("/api/users/:id", (req, res) => {
+
+    db.remove(req.params.id)
+        .then(user => {
+            if(user){
+                res.status(204).end();
+            }else{
+                res.status(404).json({ message: "The user with the specified ID does not exist" });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: "The user could not be removed" })
+        })
+});
+
+server.put("/api/users/:id", (req, res) => {
+
+    db.update(req.params.id, req.body)
+        .then(user => {
+            if(user){
+                if(user.name == null || user.bio == null){
+                    res.status(400).json({ message: "Please provide name and bio for the user" });
+                }
+                else{
+                    res.status(200).json(user);
+                }
+            }else{
+                res.status(404).json({ message: "The user with the specified ID does not exist" });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: "The user information could not be modified" });
+        });
+
+})
 module.exports = server; // EXPORT YOUR SERVER instead of {}
